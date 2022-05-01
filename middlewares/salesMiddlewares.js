07e00c5261bb8b207ae00cd94862064a6dbdedbd
itaji-create <itaji.1999@gmail.com>
2022-04-30 22:28:13 -0300

    adiciona validação para que não haja a venda de uma quantidade de produtos maior que o disponivel

--- middlewares/salesMiddlewares.js
+++ middlewares/salesMiddlewares.js
@@ -1,15 +1,35 @@
+const ProductsModel = require('../models/products');
+
 const validProductId = (req, res, next) => {
-  const { productId } = req.body[0];
-  if (!productId) return res.status(400).json({ message: '"productId" is required' });
+  const { body } = req;
+  const und = body.some(({ productId }) => productId === '');
+  if (und) return res.status(400).json({ message: '"productId" is required' });
+  
+  const zero = body.some(({ productId }) => productId < 1);
+  if (zero) {
+    return res.status(422).json({ message: '"productId" must be greater than or equal to 1' });
+  }    
   next();
 };
 
-const validProductQuantity = (req, res, next) => {
-  const { quantity } = req.body[0];
-  if (quantity === undefined) return res.status(400).json({ message: '"quantity" is required' });
-  if (quantity < 1) {
+const validProductQuantity = async (req, res, next) => {
+  const { body } = req;
+  const products = await ProductsModel.getAll();
+  const und = body.some(({ quantity }) => quantity === '');
+  if (und) return res.status(400).json({ message: '"quantity" is required' });
+  
+  const zero = body.some(({ quantity }) => quantity < 1);
+  if (zero) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-  }
+  } 
+   
+  body.forEach(({ productId, quantity }) => {
+    const product = products.find((i) => i.id === productId);
+    if (quantity > product.quantity) {
+      return res.status(422).json({ message: 'Such amount is not permitted to sell' });
+    }  
+  });
+
   next();
 };
 
